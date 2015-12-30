75a34d572a7111fe6da1e2746fffa64166bdc9e8
Jonas <jonas.dagefoerde@posteo.de>
2015-12-30 16:03:33 +0100

    Changed ajax call to POST

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 
 (function(ext) {
   // TODO: login to receive access token
-  var access_token = "de4187afe811a8fa678ba097c2d1efecfcb6f442";
+  var usr_access_token = "de4187afe811a8fa678ba097c2d1efecfcb6f442";
   // TODO: set device id via hat block
   var device_id = "1c0035001247343432313031";
 
@@ -15,19 +15,22 @@
   };
 
   ext.move_forward = function(location, callback) {
-    // Make an AJAX call to the Open Weather Maps API
-    Console.log("get_temp started...");
+    // Make an AJAX call to the Particle API
+    Console.log("move_forward started: https://api.particle.io/v1/devices/" + device_id + "/move_fwd...");
     $.ajax({
+      type: "POST",
       url: 'https://api.particle.io/v1/devices/' + device_id + '/move_fwd ' +
         '-H "Authorization: Bearer ' + access_token + '"' +
         '-d arg=""',
-      dataType: 'jsonp',
-      success: function( func_response ) {
+      dataType: 'json',
+      data: { access_token: usr_access_token, args: "" },
+      success: function() { $("#result").text("Success"); },
+//      success: function( func_response ) {
         // Got the data - parse it and return the function's return value
-        return_value = func_response['return_value'];
-        callback(return_value);
-      }
+        //return_value = func_response['return_value'];
+//      }
     });
+    console.log("move_forward stopped.");
   };
 
   // Block and block menu descriptions
