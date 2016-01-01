0a2da04e43179b25b78edfaa55eb0b24962a4064
Jonas <jonas.dagefoerde@posteo.de>
2016-01-01 17:06:59 +0100

    Testing async api calls

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,7 +21,7 @@
     $.ajax({
       type: "POST",
       url: 'https://api.particle.io/v1/devices/' + device_id + '/' + func_name,
-      async: false,
+      async: true,
       dataType: 'json',
       data: { access_token: usr_access_token, args: "" },
       success: function() {}
