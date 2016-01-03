d81af09e7cd9b952cab956226a311a184cb0b045
Jonas <jonas.dagefoerde@posteo.de>
2016-01-03 21:21:14 +0100

    Fixing param block

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,8 +32,8 @@
 
   ext.move_forward = function() { particle_api_call("move", "fwd"); };
   ext.move_backward = function() { particle_api_call("move", "bkw"); };
-  ext.move_left = function(duration) { particle_api_call("move", "left " + duration); };
-  ext.move_right = function(duration) { particle_api_call("move", "right " + duration); };
+  ext.move_left = function(duration) { particle_api_call("move", "left"); };
+  ext.move_right = function(duration) { particle_api_call("move", "right"); };
 
   // Block and block menu descriptions
   var descriptor = {
